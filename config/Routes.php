16809e95b47618586fb8d86ab41837c263d9c2b2
Bruno Bortolli <bbortolli2@hotmail.com>
2019-09-02 14:29:23 -0300

    routing

--- config/Routes.php
+++ config/Routes.php
@@ -4,9 +4,21 @@ $myRoutes = [
     'User' => [
         'getData' => 'GET',
         'addData' => 'POST',
+        'updateData' => 'PUT',
+        'removeData' => 'DELETE'
+    ],
+    'Rinha' => [
+        'getData' => 'GET',
+        'addData' => 'POST',
+        'updateData' => 'PUT',
+        'removeData' => 'DELETE'
+    ],
+    'Vote' => [
+        'getAllVotes' => 'GET',
+        'addData' => 'POST',
         'updateAccount' => 'PUT',
         'removeAccount' => 'DELETE'
-    ]
+    ],
 ];
 
 ?>
