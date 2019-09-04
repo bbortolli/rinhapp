d29f1e9bafef925a5c9c7d596a87971ffe30ffd3
Bruno Bortolli <bbortolli2@hotmail.com>
2019-09-04 15:33:52 -0300

    db

--- gen.sql
+++ gen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 02, 2019 at 05:40 PM
+-- Generation Time: Sep 04, 2019 at 06:33 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.3.7
 
@@ -22,6 +22,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `rinhas` (
   `_id` int(11) NOT NULL,
+  `owner` int(11) NOT NULL,
   `team1` varchar(255) NOT NULL,
   `team2` varchar(255) NOT NULL,
   `endtime` datetime NOT NULL,
@@ -29,13 +30,6 @@ CREATE TABLE `rinhas` (
   `totalteam1` bigint(20) NOT NULL DEFAULT '0',
   `totalteam2` bigint(20) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `rinhas`
---
-
-INSERT INTO `rinhas` (`_id`, `team1`, `team2`, `endtime`, `finished`, `totalteam1`, `totalteam2`) VALUES
-(1, 'palmeiras', 'corinthians', '2019-09-30 00:00:00', 0, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -46,15 +40,16 @@ INSERT INTO `rinhas` (`_id`, `team1`, `team2`, `endtime`, `finished`, `totalteam
 CREATE TABLE `users` (
   `_id` int(11) NOT NULL,
   `nickname` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `token` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`_id`, `nickname`, `password`) VALUES
-(2, 'teste', '123');
+INSERT INTO `users` (`_id`, `nickname`, `password`, `token`) VALUES
+(3, 'teste', '123', '');
 
 -- --------------------------------------------------------
 
@@ -75,7 +70,45 @@ CREATE TABLE `votes` (
 --
 
 INSERT INTO `votes` (`_id`, `userid`, `rinhaid`, `timevoted`, `teamvoted`) VALUES
-(1, 2, 1, '2019-09-02 16:14:13', 'first');
+(1, 2, 1, '2019-09-02 16:14:13', 'first'),
+(2, 2, 1, '2019-09-03 15:32:40', 'first'),
+(3, 2, 1, '2019-09-03 15:32:44', 'secnd'),
+(4, 2, 1, '2019-09-03 15:32:47', 'secnd'),
+(5, 2, 1, '2019-09-03 15:48:16', 'secnd'),
+(6, 2, 1, '2019-09-03 15:51:06', 'first'),
+(7, 2, 1, '2019-09-03 15:51:49', 'first'),
+(8, 2, 1, '2019-09-03 15:52:26', 'first'),
+(9, 2, 1, '2019-09-03 15:52:40', 'first'),
+(10, 2, 1, '2019-09-03 15:52:57', 'first'),
+(11, 2, 1, '2019-09-03 15:53:06', 'first'),
+(12, 2, 1, '2019-09-03 15:53:46', 'first'),
+(13, 2, 1, '2019-09-03 15:54:12', 'first'),
+(14, 2, 1, '2019-09-03 15:54:28', 'first'),
+(15, 2, 1, '2019-09-03 15:55:16', 'first'),
+(16, 2, 1, '2019-09-03 15:55:18', 'first'),
+(17, 2, 1, '2019-09-03 15:55:19', 'first'),
+(18, 2, 1, '2019-09-03 15:55:20', 'secnd'),
+(19, 2, 1, '2019-09-03 15:55:20', 'secnd'),
+(20, 2, 1, '2019-09-03 15:55:21', 'secnd'),
+(21, 2, 1, '2019-09-03 15:55:21', 'secnd'),
+(22, 2, 1, '2019-09-03 15:56:03', 'secnd'),
+(23, 2, 1, '2019-09-03 15:56:04', 'secnd'),
+(24, 2, 1, '2019-09-03 15:56:05', 'secnd'),
+(25, 2, 1, '2019-09-03 16:16:52', 'secnd'),
+(26, 2, 1, '2019-09-03 16:16:52', 'secnd'),
+(27, 2, 1, '2019-09-03 16:16:53', 'secnd'),
+(28, 2, 1, '2019-09-03 16:16:54', 'secnd'),
+(29, 2, 1, '2019-09-03 16:16:54', 'secnd'),
+(30, 2, 1, '2019-09-03 16:16:54', 'secnd'),
+(31, 2, 1, '2019-09-03 16:16:55', 'secnd'),
+(32, 2, 1, '2019-09-03 16:17:30', 'secnd'),
+(33, 2, 1, '2019-09-03 16:17:30', 'secnd'),
+(34, 2, 1, '2019-09-03 16:17:31', 'secnd'),
+(35, 2, 1, '2019-09-03 16:17:32', 'first'),
+(36, 2, 1, '2019-09-03 16:17:32', 'first'),
+(37, 2, 1, '2019-09-03 16:17:32', 'first'),
+(38, 2, 1, '2019-09-03 16:17:32', 'first'),
+(39, 2, 1, '2019-09-03 16:17:32', 'first');
 
 --
 -- Triggers `votes`
@@ -109,7 +142,8 @@ DELIMITER ;
 -- Indexes for table `rinhas`
 --
 ALTER TABLE `rinhas`
-  ADD PRIMARY KEY (`_id`);
+  ADD PRIMARY KEY (`_id`),
+  ADD KEY `fk_owner_userid` (`owner`);
 
 --
 -- Indexes for table `users`
@@ -137,10 +171,20 @@ ALTER TABLE `rinhas`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `rinhas`
+--
+ALTER TABLE `rinhas`
+  ADD CONSTRAINT `fk_owner_userid` FOREIGN KEY (`owner`) REFERENCES `users` (`_id`) ON DELETE CASCADE ON UPDATE CASCADE;
